3f042433426c8bb953f169ef33a601863a69a29e
JorgeRochaA <jor2019.cr@gmail.com>
2023-04-25 17:00:52 -0600

    feat add users entity

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,6 +1,43 @@
-import { Entity } from "typeorm"
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class User {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
 
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column({ default: () => "now()" })
+    createdAt: Date;
+
+    @Column({ default: () => "now()" })
+    updatedAt: Date;
+
+    constructor(
+        id: string,
+        firstName: string,
+        lastName: string,
+        email: string,
+        password: string,
+        createdAt:string,
+        updatedAt:string
+    ) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.createdAt = new Date(createdAt);
+        this.updatedAt = new Date(updatedAt);
+    }
 }
