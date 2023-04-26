59a0aa61b2c6727eea89a0684210feb7d77794cf
JorgeRochaA <jor2019.cr@gmail.com>
2023-04-26 12:45:52 -0600

    refactor : add try catch

--- src/controllers/todos.controller.ts
+++ src/controllers/todos.controller.ts
@@ -3,19 +3,19 @@ import { Todos } from "../entities/Todos";
 
 export const createTodo = async (ctx: Context) => {
   //const { title, priority_index } = ctx.request.body;
-  const lastIndex = (await Todos.count()) + 1;
-  const todoCreated = await Todos.create({
-    title: `Tarea #${lastIndex}`,
-    priority_index: lastIndex,
-  }).save();
+  try {
+    const lastIndex = (await Todos.count()) + 1;
+    await Todos.create({
+      title: `Tarea #${lastIndex}`,
+      priority_index: lastIndex,
+    }).save();
 
-  if (!todoCreated) {
+    ctx.response.status = 200;
+    ctx.body = { message: "Todo created successfully" };
+  } catch (error) {
     ctx.status = 500;
     ctx.body = { message: "Error creating todo" };
   }
-
-  ctx.response.status = 200;
-  ctx.body = { message: "Todo created successfully" };
 };
 
 export const getTodos = async (ctx: Context) => {
@@ -25,36 +25,41 @@ export const getTodos = async (ctx: Context) => {
 };
 
 export const getTodo = async (ctx: Context) => {
-  const todo = await Todos.findOne({ where: { id: ctx.params.id } });
+  try {
+    const todo = await Todos.findOne({ where: { id: ctx.params.id } });
 
-  if (!todo) {
-    ctx.status = 404;
-    ctx.body = { message: "Todo not found" };
-    return;
+    if (!todo) {
+      ctx.status = 404;
+      ctx.body = { message: "Todo not found" };
+      return;
+    }
+
+    ctx.response.status = 200;
+    ctx.body = { todo };
+  } catch (error) {
+    ctx.status = 500;
+    ctx.body = { message: "Error getting todo" };
   }
-
-  ctx.response.status = 200;
-  ctx.body = { todo };
 };
 
 export const updateTodo = async (ctx: Context) => {};
 
 export const deleteTodo = async (ctx: Context) => {
-  const todo = await Todos.findOne({ where: { id: ctx.params.id } });
+  try {
+    const todo = await Todos.findOne({ where: { id: ctx.params.id } });
 
-  if (!todo) {
-    ctx.status = 404;
-    ctx.body = { message: "Todo not found" };
-    return;
-  }
+    if (!todo) {
+      ctx.status = 404;
+      ctx.body = { message: "Todo not found" };
+      return;
+    }
 
-  const todoRemoved = await todo.remove();
+    await todo.remove();
 
-  if (!todoRemoved) {
+    ctx.response.status = 200;
+    ctx.body = { message: "Todo deleted successfully" };
+  } catch (error) {
     ctx.status = 500;
     ctx.body = { message: "Error deleting todo" };
   }
-
-  ctx.response.status = 200;
-  ctx.body = { message: "Todo deleted successfully" };
 };
